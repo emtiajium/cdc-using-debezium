2f3675610fa24a75ce532bf506ad7f74761f262d
Emtiaj Hasan <271emtiaj@gmail.com>
2023-08-25 15:44:24 +0600

    added Kafka commands

--- kafka.bash
+++ kafka.bash
@@ -1,1 +1,8 @@
-docker exec cdc-using-debezium-kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --list;
+server=localhost:9092
+topic=cdc-using-debezium-topic.public.User
+
+docker exec cdc-using-debezium-kafka sh -c 'cd /opt/bitnami/kafka/bin && ls'
+
+docker exec cdc-using-debezium-kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server $server --list
+
+docker exec cdc-using-debezium-kafka /opt/bitnami/kafka/bin/kafka-console-consumer.sh --bootstrap-server $server --topic $topic --from-beginning
