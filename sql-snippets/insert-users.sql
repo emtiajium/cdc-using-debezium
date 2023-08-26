acd26ed82b7d24827040800867b2ce3fcdcb4a42
Emtiaj Hasan <271emtiaj@gmail.com>
2023-08-26 16:49:38 +0600

    added update and delete queries

--- sql-snippets/insert-users.sql
+++ sql-snippets/insert-users.sql
@@ -2,3 +2,14 @@ INSERT INTO "User" (email, name)
 VALUES ('ehasan+1@firecrackervocabulary.com', CONCAT('name_', (random() * 1000)::INTEGER::VARCHAR))
 ON CONFLICT (email) DO UPDATE SET name = EXCLUDED.name
 RETURNING *;
+
+INSERT INTO "User" (email, name)
+VALUES ('ehasan+2@firecrackervocabulary.com', 'name_2');
+
+UPDATE "User"
+SET name = 'name_20'
+WHERE email = 'ehasan+2@firecrackervocabulary.com';
+
+DELETE
+FROM "User"
+WHERE email = 'ehasan+2@firecrackervocabulary.com';
